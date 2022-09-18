b04e283e9a4c523fb2aa533abdd82157864f28d4
LEDUNOIS Simon <simon.ledunois@gmail.com>
2022-09-18 19:33:43 +0000

    chore: move config into test package

--- pkg/test/test.go
+++ pkg/test/test.go
@@ -8,3 +8,13 @@ type Test struct {
 	Mock      func()
 	Validator func(t *testing.T, value interface{}, err error)
 }
+
+// DefaultAPIKey returns the default testing api key
+func DefaultAPIKey() string {
+	return "supersecret"
+}
+
+// DefaultSecret returns the default testing secret
+func DefaultSecret() string {
+	return "supersecret"
+}
